82bf0c1f013248ce823d19a5e2547398a46ddc11
KombinatorVit <slonik0775@gmail.com>
2023-01-24 21:54:19 +0200

    Реализуем логику переключения светлой и темной темы с помощью material ui createTheme

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,32 @@ import Home from "./components/home";
 import {Route, Routes} from "react-router-dom";
 import PrivateRoute from "./utils/router/privateRoute";
 import AuthRootComponent from "./components/auth";
+import {ColorModeContext, useMode} from './theme'
+import {CssBaseline, ThemeProvider} from '@mui/material'
+
 
 function App() {
-    return (
-        <div className="App">
-            <Home/>
-            <Routes>
-                <Route element={<PrivateRoute/>}>
-                    <Route path='/' element={<Home/>}/>
 
-                </Route>
-                <Route path='login' element={<AuthRootComponent/>}/>
-                <Route path='register' element={<AuthRootComponent/>}/>
-            </Routes>
-        </div>
+    const [theme, colorMode] = useMode()
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline/>
+                <div className="App">
+                    <Home/>
+                    <Routes>
+                        <Route element={<PrivateRoute/>}>
+                            <Route path='/' element={<Home/>}/>
+
+                        </Route>
+                        <Route path='login' element={<AuthRootComponent/>}/>
+                        <Route path='register' element={<AuthRootComponent/>}/>
+                    </Routes>
+                </div>
+            </ThemeProvider>
+
+        </ColorModeContext.Provider>
     );
 }
 
