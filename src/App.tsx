64f21051772afb516292507f734f5414879a9933
KombinatorVit <slonik0775@gmail.com>
2023-03-04 23:38:29 +0200

    Implement Search logic - Add styles to single asset page - Implement request to backend 'watchlist/create'

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import {Route, Routes} from "react-router-dom";
 import PrivateRoute from "./utils/router/privateRoute";
-import AuthRootComponent from "./pages/auth";
 import {ColorModeContext, useMode} from './theme'
 import {CssBaseline, ThemeProvider} from '@mui/material'
 import LayoutComponent from "./components/Layout";
 import WatchlistPage from "./pages/watchlist";
 import HomePage from "./pages/home";
-import SettingsPage from "./pages/settings";
 import NewsPage from "./pages/news";
 import AuthRootPage from "./pages/auth";
 import SingleAssetPage from "./pages/single-asset";
+import SettingsPage from "./pages/settings";
 
 
 function App() {
@@ -28,6 +27,10 @@ function App() {
                                 <Route path="/" element={<HomePage/>}/>
                                 <Route path="/watchlist" element={<WatchlistPage/>}/>
                                 <Route path="/news" element={<NewsPage/>}/>
+                                <Route
+                                    path="/settings"
+                                    element={<SettingsPage/>}
+                                />
                                 <Route path="/single/:id" element={<SingleAssetPage/>}/>
                             </Route>
                             <Route path="login" element={<AuthRootPage/>}/>
