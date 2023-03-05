739223fe010309b09d4ab6eca27f109085df493d
KombinatorVit <slonik0775@gmail.com>
2023-03-05 16:53:09 +0200

    Implement Watchlist logic - Create Watchlist component - Get data from backend - Filtered assets array - Render assets array on watchlist page

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,15 @@
-import {configureStore} from "@reduxjs/toolkit";
-import authSlice from "./slice/auth";
-import assetSlice from "./slice/assets";
-
+import { configureStore } from '@reduxjs/toolkit'
+import authSlice from './slice/auth'
+import assetSlice from './slice/assets'
+import watchlistSlice from './slice/watchlist'
 
 const store = configureStore({
     reducer: {
         auth: authSlice,
-        assets: assetSlice
-    }
+        assets: assetSlice,
+        watchlist: watchlistSlice,
+    },
 })
-
-
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
