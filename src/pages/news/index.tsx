c70d16a3a3a9b0f0b972a607d2cb93155ddc9588
KombinatorVit <slonik0775@gmail.com>
2023-03-06 22:58:27 +0200

    Implement News logic
    - Create news component logic
    - Get news data from cryptocompare api
    - Render News record
    
    Implement alert logic in single asset page

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, {useEffect} from 'react'
+import {getNews} from '../../store/thunks/news'
+import {Box, Grid, Link, Typography} from '@mui/material'
+import {useStyles} from './styles'
+import {useAppDispatch, useAppSelector} from "../../utils/hooks";
 
 const NewsPage = () => {
-    return (
-        <div>
-            <h1>News page</h1>
-        </div>
-    );
-};
+    const dispatch = useAppDispatch()
+    const {news} = useAppSelector((state) => state.news)
+    const classes = useStyles()
 
-export default NewsPage;
+    const renderNewsBlock = news.map((element: any) => (
+        <Grid container className={classes.newsBlock}>
+            <Grid item xs={12} md={3}>
+                <img src={element.imageurl} alt={element.category}/>
+            </Grid>
+            <Grid item xs={12} md={9}>
+                <Box className={classes.newsTitle}>
+                    <Typography variant="h3">{element.title}</Typography>
+                </Box>
+                <Box>
+                    <Typography variant="body1">{element.body}</Typography>
+                </Box>
+            </Grid>
+            <Grid item xs={12} md={12} className={classes.readMore}>
+                <Typography variant="h4">
+                    <Link target={'_blank'} href={element.url}>Read more</Link>
+                </Typography>
+            </Grid>
+        </Grid>
+    ))
+
+    useEffect(() => {
+        dispatch(getNews())
+    }, [])
+
+    return (
+        <Grid className={classes.root}>
+            <Grid className={classes.blockTitle}>
+                <Typography variant="h2">Новости</Typography>
+            </Grid>
+            <Grid>{renderNewsBlock}</Grid>
+        </Grid>
+    )
+}
+
+export default NewsPage
